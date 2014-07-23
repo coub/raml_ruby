2dc0ea34bed5631f1c0f88f8b37a6c11809e9bfd
Elias Levy <fearsome.lucidity@gmail.com>
2014-07-23 09:26:57 -0700

    Make required attribute default to true for URI parameters.

--- lib/raml/parameter/uri_parameter.rb
+++ lib/raml/parameter/uri_parameter.rb
@@ -1,7 +1,11 @@
 module Raml
   module Parameter
     class UriParameter < AbstractParameter
-
+      def validate
+        # required default to true for URI parameters
+        @required = true if required.nil?
+        super
+      end
     end
   end
 end
--- test/raml/parameter/uri_parameter_spec.rb
+++ test/raml/parameter/uri_parameter_spec.rb
@@ -12,9 +12,17 @@ describe Raml::Parameter::UriParameter do
 
   subject { Raml::Parameter::UriParameter.new(name, data) }
 
-
-  it "should instanciate Uri parameter" do
-    Raml::Parameter::UriParameter.new(name, data)
+  describe '#new' do
+    it "should instanciate Uri parameter" do
+      Raml::Parameter::UriParameter.new(name, data)
+    end
+    
+    context 'when no required attribute is given' do
+      let(:data) { { } }
+      it 'defaults to true' do
+        subject.required.should == true
+      end
+    end
   end
 
   describe "#document" do
