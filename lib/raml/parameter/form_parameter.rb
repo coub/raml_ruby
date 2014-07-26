5194c99185700dc8171cfc7914f0f1453a84d8a7
Elias Levy <fearsome.lucidity@gmail.com>
2014-07-25 18:26:27 -0700

    Seems done.

--- lib/raml/parameter/form_parameter.rb
+++ lib/raml/parameter/form_parameter.rb
@@ -1,10 +1,6 @@
 module Raml
   module Parameter
     class FormParameter < AbstractParameter
-      def initialize(name, form_parameter_data)
-        puts "FORM PARAMETER is partially implemented"
-        super
-      end
     end
   end
 end
