cb4903116579f086632810300489234cd5eb8c7a
kgorin <machineshopflow@gmail.com>
2015-04-13 13:04:43 +0300

    Fixed InvalidProperty error message typo.

--- lib/raml/mixin/documentable.rb
+++ lib/raml/mixin/documentable.rb
@@ -17,11 +17,11 @@ module Raml
     end
 
     def validate_display_name
-      raise InvalidProperty, "displayName property mus be a string." unless display_name.is_a? String
+      raise InvalidProperty, "displayName property must be a string." unless display_name.is_a? String
     end
 
     def validate_description
-      raise InvalidProperty, "description property mus be a string." unless description.is_a? String
+      raise InvalidProperty, "description property must be a string." unless description.is_a? String
     end
   end
 end
